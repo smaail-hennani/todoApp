e3810ef553616f73c20d0d90bf2a8799d0c043aa
gylgamesh34 <gylgamesh34x@gmail.com>
2025-03-18 16:02:37 +0100

    feat(appConfig): ajout de la configuration de ngx-translate pour la gestion i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule, withFetch } from '@angular/common/http';
+import { HttpClient, HttpClientModule, withFetch } from '@angular/common/http';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './core/interceptor/token.interceptor';
 import { JWT_OPTIONS, JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export function jwtOptionsFactory() {
   return {
@@ -15,6 +17,10 @@ export function jwtOptionsFactory() {
     allowedDomains: ['localhost:4200', 'https://backend.osc-fr1.scalingo.io/', 'localhost:4000'],
     disallowedRoutes: ['localhost:4200/login', 'localhost:4200/register' ],
   };
+}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 export const appConfig: ApplicationConfig = {
@@ -28,6 +34,14 @@ export const appConfig: ApplicationConfig = {
       provide: JWT_OPTIONS,
       useFactory: jwtOptionsFactory,
     },
-    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    ),
   ]
 };
